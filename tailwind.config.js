6150be5ec230de0673ed5807c004ead4e2add783
Tristan NGUYEN <trungupdate@gmail.com>
2025-03-20 02:32:40 +0700

    Add configuration files for Tailwind CSS and TypeScript

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                'neo-primary': '#7209b7',
+                'neo-secondary': '#3a0ca3',
+                'neo-accent': '#4cc9f0',
+            },
+        },
+    },
+    plugins: [],
+}
